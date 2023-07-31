310dac5a63cadcc0b55056922ed66c8e3ad1b8d8
nobru01 <brunoferreira01@gmail.com>
2023-07-31 11:03:45 -0300

    contexto para o html e for em python

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados={
+        1: {
+            'nome':'Nome_foto_1',
+            'legenda':'Fonte_1'
+        },
+        2: {
+            'nome':'Nome_foto_2',
+            'legenda':'Fonte_2'
+        },
+    }
+    return render(request, 'galeria/index.html',{'cards':dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
