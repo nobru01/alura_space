9e14e69faa42fde8a44327808a411ca50fbc8d27
nobru01 <brunoferreira01@gmail.com>
2023-08-07 12:43:04 -0300

    Busca no DB a lista e um objeto específico

--- galeria/views.py
+++ galeria/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
+from galeria.models import Fotografia
 
 def index(request):
 
-    dados={
-        1: {
-            'nome':'Nome_foto_1',
-            'legenda':'Fonte_1'
-        },
-        2: {
-            'nome':'Nome_foto_2',
-            'legenda':'Fonte_2'
-        },
-    }
-    return render(request, 'galeria/index.html',{'cards':dados})
+    fotografia=Fotografia.objects.all()
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    return render(request, 'galeria/index.html',{'cards':fotografia})
+
+def imagem(request,foto_id):
+
+    # 1º - Tudo começou com a montagem do caminho no index.html passando tbm um parâmetro id na url
+    # 2º - Captura do parâmetro id na url /<int:foto_id> (alteração do urls.py)
+    # 3º - Inserção do parâmetro adicional da função da views -> retorno do objeto para a imagem.html
+
+
+    fotografia=get_object_or_404(Fotografia,pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia':fotografia})
